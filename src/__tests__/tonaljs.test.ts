9afc29e08e6118e5c078582793eceb38c5b8fd96
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-12-10 15:18:43 +0100

    Add more tonaljs exploratory tests.

--- src/__tests__/tonaljs.test.ts
+++ src/__tests__/tonaljs.test.ts
@@ -1,4 +1,4 @@
-import { Note, Scale } from "tonal";
+import { Interval, Note, Scale } from "tonal";
 
 describe("Tonal", () => {
     it("decodes a note", () => {
@@ -20,5 +20,40 @@ describe("Tonal", () => {
         expect(scale.notes.length).toEqual(7);
         expect(scale.tonic).toEqual("B5");
     });
-});
 
+    it("finds ascending or descending intervals", () => {
+        const g5 = Note.get("G5")
+        const gs5 = Note.get("G#5")
+        const a5 = Note.get("A5")
+        const b5 = Note.get("B5")
+
+        if (g5.empty) return;
+        if (gs5.empty) return;
+        if (a5.empty) return;
+        if (b5.empty) return;
+
+        expect(Note.ascending(g5, gs5)).toEqual(-1);
+        expect(Note.ascending(g5, gs5)).toEqual(-1);
+        expect(Note.ascending(g5, a5)).toEqual(-2);
+        expect(Note.ascending(g5, b5)).toEqual(-4);
+    });
+
+    it("can add intervals", () => {
+        const firstNote = "G5";
+        const interval = 12;
+
+        const result = Note.transpose(firstNote, Interval.fromSemitones(interval));
+
+        expect(result).toEqual("G6");
+    });
+
+    it("can find intervals", () => {
+        const firstNote = "C5";
+        const perfectFifth = "G5";
+
+        expect(Interval.distance(firstNote, perfectFifth)).toEqual("5P");
+
+        const majorSixth = "A5";
+        expect(Interval.distance(firstNote, majorSixth)).toEqual("6M");
+    });
+});
