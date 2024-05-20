f6356303c18d33274207124fab23bb5ee70bbe3a
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2024-05-20 10:22:08 +0200

    Build step: Create manual chunks for some dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'rendering': ['react', 'react-dom', 'styled-components'],
+                    'music': ['tonal', 'vexflow']
+                }
+            }
+        }
+    }
 })
